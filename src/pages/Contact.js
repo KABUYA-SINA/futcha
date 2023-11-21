e783d4f15e38939e15f3158801bae1072844d5a5
KABUYA <kabuya-sina@netcourrier.com>
2023-11-21 17:37:46 +0100

    V .1.1

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../sass/base/_base.scss';
+import '../sass/base/_font.scss';
+import '../sass/layout/_container.scss';
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='container'>
+            <Header />
+            <Footer />
+        </div>
     )
 }
 
--- src/pages/Future.js
+++ src/pages/Future.js
@@ -1,8 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import images from '../assets/pages/futcha.webp';
+import Collapse from '../components/Collapse';
+import '../sass/base/_base.scss';
+import '../sass/base/_font.scss';
+import '../sass/layout/_container.scss';
+import '../sass/pages/_futcha.scss';
 
 const Future = () => {
+    const [img, setImg] = useState([])
+    useEffect(() => (
+        setImg(images)
+    ), [])
     return (
-        <div>Future</div>
+        <div className='container'>
+            <Header />
+            <main className='fut-main'>
+                <div className="fit--img">
+                    <img src={img} alt='future' />
+                </div>
+                <div className="fit--text">
+                    <h1>Future</h1>
+                    <p>
+                        Il est difficile de prédire l'avenir avec certitude, car de nombreux facteurs peuvent influencer le cours des événements.
+                        Cependant, on peut s'attendre à ce que plusieurs tendances et développements actuels continuent à façonner l'avenir.
+                        Voici quelques domaines clés qui pourraient jouer un rôle important dans le futur :
+                    </p>
+                    <div className='text-fut'>
+                        <ul>
+                            <li>
+                                <span className="heading">1. Technologie: </span>
+                                <span> Les avancées rapides dans les domaines tels que l'intelligence artificielle, l'informatique quantique, la biotechnologie et l'automatisation continueront probablement à façonner notre façon de vivre et de travailler.</span>
+                            </li>
+                            <li>
+                                <span className="heading">2. Durabilité: </span>
+                                <span> La prise de conscience croissante des enjeux environnementaux pourrait conduire à des changements significatifs dans la façon dont nous produisons et consommons des biens et des services, avec un accent accru sur la durabilité.</span>
+                            </li>
+                            <li>
+                                <span className="heading">3. Santé: </span>
+                                <span> Les progrès dans la médecine, les soins de santé personnalisés et la recherche sur les maladies pourraient améliorer la qualité de vie et prolonger l'espérance de vie.</span>
+                            </li>
+                            <li>
+                                <span className="heading">4. Économie: </span>
+                                <span> Les modèles économiques peuvent évoluer en réponse aux changements technologiques, aux perturbations industrielles et aux défis mondiaux, tels que les pandémies.</span>
+                            </li>
+                            <li>
+                                <span className="heading">5. Climat: </span>
+                                <span> Les efforts pour atténuer le changement climatique et s'adapter à ses effets pourraient devenir des priorités majeures à l'échelle mondiale.</span>
+                            </li>
+                            <li>
+                                <span className="heading">6. Société: </span>
+                                <span> Les questions liées à l'équité, à la diversité et à l'inclusion pourraient devenir de plus en plus importantes, influençant les politiques, les normes sociales et les relations interpersonnelles.</span>
+                            </li>
+                            <li>
+                                <span className="heading">7. Géopolitique: </span>
+                                <span> Les relations entre les nations, les alliances et les conflits pourraient évoluer en réponse à des facteurs tels que les changements économiques, les évolutions technologiques et les défis mondiaux.</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <p>
+                        Il est important de noter que ces points sont généraux, et les événements réels peuvent être influencés par une combinaison complexe de facteurs.
+                        L'avenir dépendra également des choix que nous faisons en tant que société et de notre capacité à résoudre les défis auxquels nous sommes confrontés.
+                    </p>
+                </div>
+            </main>
+            <Footer />
+        </div>
     )
 }
 export default Future;
