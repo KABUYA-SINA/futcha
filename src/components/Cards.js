5428fa93e4503fa5bd484f9e0e45e33dea6f2708
KABUYA <kabuya-sina@netcourrier.com>
2023-12-01 17:04:59 +0100

    v .1.7

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Collapse from './Collapse';
 import '../sass/pages/_cards.scss'
 
 const Cards = ({ id, cover, title, alt, source, text }) => {
+    const [imgAppear, setImgAppear] = useState('')
+    const empty = cover ? <img src={cover} alt={alt} /> : ''
+
+    useEffect(() => {
+        setImgAppear(true)
+    }, [])
+
     return (
         <div key={id} className="card">
-            <img src={cover} alt={alt} />
+            {imgAppear && empty}
             <Collapse title={title} text={text} source={source} />
         </div>
     )
